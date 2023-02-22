c869301bb8bcdfdd6d3749cd4d2ee9ab9fb5914e
jakubkania91 <jakub.kania91@interia.pl>
2023-02-22 20:29:06 +0100

    new exercises tematyka6

--- src/tematyka6/Car.java
+++ src/tematyka6/Car.java
@@ -15,10 +15,73 @@ public class Car {
 
     }
 
-    String brand;
-    String model;
-    String color;
-    int year;
-    int doors;
+    private String brand;
+    private String model;
+    private String color;
+    private int year;
+    private int doors;
 
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public String getBrand() {
+        return this.brand;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public int getDoors() {
+        return this.doors;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", year=" + year +
+                ", doors=" + doors +
+                '}';
+    }
+    @Override
+    public boolean equals(Object object){
+        if (this==object){
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Car car = (Car) object;
+        return brand.equals(car.getBrand()) && model.equals(car.getModel()) && color.equals(car.getColor()) && year==car.getYear() && doors==car.getDoors();
+
+    }
 }
--- src/tematyka6/Main.java
+++ src/tematyka6/Main.java
@@ -4,18 +4,31 @@ public class Main {
     public static void main(String[] args) {
 
         Car car = new Car();
-        car.brand = "mercede";
+     /*   car.brand = "mercede";
         car.color = "red";
         car.model = "slk";
         car.year = 2006;
         car.doors = 5;
 
         System.out.println(car.brand + " " + car.model + " " + car.color + " " + car.year + " " + car.doors);
-
+*/
         Car seckondCar = new Car("bmw", "m7", "blue",5, 2011);
+        Car thirdCar = new Car("bmw", "m7", "blue",5, 2011);
+        boolean cars = seckondCar == thirdCar;
+        System.out.println(cars);
+        boolean equals = seckondCar.equals(thirdCar);
+        System.out.println(equals);
 
-        System.out.println(seckondCar.brand + " " + seckondCar.color + " " + seckondCar.model + " " + seckondCar.year + " " +seckondCar.doors);
+        car.setBrand("BMW");
+        car.setModel("M4");
+        car.setColor("Orange");
+        car.setYear(2011);
+        car.setDoors(5);
 
+        String brand = car.getBrand();
+        System.out.println(brand);
+        System.out.println(car.getModel() + " " + car.getBrand() + " " + car.getColor() + " " + car.getYear() + " " + car.getDoors());
+        System.out.println(car.toString());
 
     }
 }
